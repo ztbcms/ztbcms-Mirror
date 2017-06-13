0b13eb6482be442a01222f061a51e10e56c52377
Jayin Ton <tonjayin@gmail.com>
2017-06-13 21:57:02 +0800

    调整表结构

--- Install/Mirror.sql
+++ Install/Mirror.sql
@@ -10,10 +10,23 @@ CREATE TABLE `cms_mirror_log` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cms_mirror_task` (
+CREATE TABLE `cms_mirror_checker` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `url` int(11) NOT NULL COMMENT '网址',
   `next_time` int(11) NOT NULL COMMENT '下一次执行时间',
   `minute` int(11) NOT NULL DEFAULT '1' COMMENT '多少分钟重复一次',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `cms_mirror_alert` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(11) DEFAULT NULL COMMENT '通知方式',
+  `type_value` int(11) DEFAULT NULL COMMENT '通知方式对应值',
+  `checker_id` int(11) DEFAULT NULL COMMENT '关联检测器的ID',
+  `check_field` int(11) DEFAULT NULL COMMENT '检测字段',
+  `check_operator` int(11) DEFAULT NULL COMMENT '比较符号',
+  `check_value` int(11) DEFAULT NULL COMMENT '检测比较值',
+  `minute` int(11) DEFAULT NULL COMMENT '分钟频率',
+  `next_time` int(11) DEFAULT NULL COMMENT '下次检测时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
