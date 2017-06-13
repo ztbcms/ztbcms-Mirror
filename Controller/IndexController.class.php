324a37d942751a479a6aadd05803c7ff8cfa69c8
Jayin Ton <tonjayin@gmail.com>
2017-06-13 22:13:04 +0800

    update

---
+++ Controller/IndexController.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * author: Jayin <tonjayin@gmail.com>
+ */
+
+namespace Mirror\Controller;
+
+use Common\Controller\AdminBase;
+
+class IndexController extends AdminBase {
+
+    function checker_list(){
+        $this->display();
+    }
+
+    function create_checker(){
+        $this->display();
+    }
+
+    function do_create_checker(){
+
+    }
+
+    function edit_checker(){
+        $this->display();
+    }
+
+    function do_edit_checker(){
+
+    }
+
+    function view_checker(){
+        $this->display();
+    }
+
+    function alert_list(){
+        $this->display();
+    }
+
+    function create_alert(){
+        $this->display();
+    }
+
+    function do_create_alert(){
+
+    }
+
+    function edit_alert(){
+        $this->display();
+    }
+
+    function do_edit_alert(){
+
+    }
+
+
+
+}
