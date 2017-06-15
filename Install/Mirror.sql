d98bf763b7bab13fc84437af482b3c642271a6ca
Jayin Ton <tonjayin@gmail.com>
2017-06-15 16:38:53 +0800

    update sql

--- Install/Mirror.sql
+++ Install/Mirror.sql
@@ -1,3 +1,12 @@
+CREATE TABLE `cms_mirror_checker` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(1024) NOT NULL DEFAULT '' COMMENT '网址',
+  `next_time` int(11) NOT NULL COMMENT '下一次执行时间',
+  `minute` int(11) NOT NULL DEFAULT '1' COMMENT '多少分钟重复一次',
+  `enable` tinyint(2) NOT NULL DEFAULT '1' COMMENT '是否启用,0否,1是',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `cms_mirror_log` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `checker_id` int(11) NOT NULL COMMENT '所属Checker ID',
@@ -10,19 +19,10 @@ CREATE TABLE `cms_mirror_log` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cms_mirror_checker` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `url` varchar(1024) NOT NULL DEFAULT '' COMMENT '网址',
-  `next_time` int(11) NOT NULL COMMENT '下一次执行时间',
-  `minute` int(11) NOT NULL DEFAULT '1' COMMENT '多少分钟重复一次',
-  `enable` tinyint(2) NOT NULL DEFAULT '1' COMMENT '是否启用,0否,1是',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `cms_mirror_alert` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `type` int(11) NOT NULL COMMENT '通知方式',
-  `type_value` int(11) NOT NULL COMMENT '通知方式对应值',
+  `type` varchar(32) NOT NULL DEFAULT '' COMMENT '通知方式',
+  `type_value` varchar(64) NOT NULL DEFAULT '' COMMENT '通知方式对应值',
   `checker_id` int(11) NOT NULL COMMENT '关联检测器的ID',
   `check_field` varchar(64) NOT NULL DEFAULT '' COMMENT '检测字段',
   `check_operator` varchar(8) NOT NULL DEFAULT '' COMMENT '比较符号',
