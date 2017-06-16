a6723bfd908eee94639d475f68d6ae7fc0fa9777
Jayin Ton <tonjayin@gmail.com>
2017-06-16 10:41:22 +0800

    refactor

--- View/Index/checker_list.php
+++ View/Index/checker_list.php
@@ -4,7 +4,7 @@
     <div id="app" style="padding: 8px;">
         <div class="row">
             <div class="col-sm-6">
-                <h4>搜索</h4>
+                <h4>Checker 列表</h4>
             </div>
             <div class="col-sm-6">
                 <a href="{:U('Mirror/Index/create_checker')}" class="btn btn-success  pull-right" style="margin-left: 8px;">新增Checker</a>
@@ -13,18 +13,11 @@
         </div>
         <hr>
         <div class="search_type cc mb10">
-            消息源：<input type="text" v-model="where.target" name="" class="input">
-            发送者：<input type="text" v-model="where.sender" name="" class="input">
-            接收者：<input type="text" v-model="where.receiver" name="" class="input">
-            是否处理：<select v-model="where.process_status"  style="background: white;height: 28px;">
-                <option value="">处理状态</option>
-                <option value="0">未处理</option>
-                <option value="1">已处理</option>
-            </select>
-            是否阅读：<select v-model="where.read_status" style="background: white;height: 28px;">
-                <option value="">处理状态</option>
-                <option value="0">未读</option>
-                <option value="1">已读</option>
+            网址：<input type="text" v-model="where.url" name="" class="input">
+            启用状态：<select v-model="where.enable" style="background: white;height: 28px;">
+                <option value="">全部</option>
+                <option value="1">启用</option>
+                <option value="0">禁用</option>
             </select>
             <button @click="getList" class="btn btn-primary" style="margin-left: 8px;">搜索</button>
         </div>
@@ -62,11 +55,10 @@
                                 <span style="color: green;">是</span>
                             </template>
                             <template v-if="item.enable == 0">
-                                <span style="color: gray;">否</span>
+                                <span style="color: red;">否</span>
                             </template>
                         </td>
                         <td >
-                            <button type="button" class="btn btn-primary" @click="handleMessage(item.id)">触发处理</button>
                             <a :href="'/Mirror/Index/logs?checker_id=' + item.id" type="button" class="btn btn-primary">日志</a>
                             <a :href="'/Mirror/Index/create_alert?checker_id=' + item.id " class="btn btn-success ">新增 Alert</a>
                         </td>
@@ -100,7 +92,8 @@
                     page_count: 0,
                     total: 0,
                     where: {
-
+                        url: '',
+                        enable: ''
                     }
                 },
                 filters: {
@@ -141,24 +134,6 @@
                             }
                         })
                     },
-                    //处理消息
-                    handleMessage: function (message_id){
-                        $.ajax({
-                            url: "{:U('Message/Message/handleMessage')}",
-                            data: {
-                                message_id: message_id
-                            },
-                            dataType: 'json',
-                            type: 'post',
-                            success: function (res) {
-                                if(res.status){
-                                    layer.msg('操作完成！');
-                                }else{
-                                    layer.msg(res.msg);
-                                }
-                            }
-                        });
-                    }
                 },
                 mounted: function () {
                     this.getList();
